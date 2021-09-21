86a975f4235ce0637b915820ad1c120549c43100
Leonard Bruns <leonardb@kth.se>
2021-09-21 12:57:46 +0200

    Add initialization of category specific pipeline to REAL275 evaluation script

--- sdf_estimation/sdf_estimation/scripts/real275_evaluation.py
+++ sdf_estimation/sdf_estimation/scripts/real275_evaluation.py
@@ -5,12 +5,14 @@ https://github.com/hughw19/NOCS_CVPR2019
 https://github.com/xuchen-ethz/neural_object_fitting
 """
 import argparse
+import copy
 import os
 import pickle
 import matplotlib.pyplot as plt
 
 import yoco
 
+from sdf_estimation.simple_setup import SDFPipeline
 from sdf_estimation.scripts.real_data import load_real275_rgbd
 
 
@@ -26,9 +28,16 @@ def main() -> None:
     parser.add_argument("--out_folder", required=True)
     config = yoco.load_config_from_args(parser)
 
-    categories = ['bottle','bowl','camera','can','laptop','mug']
-    model_dict = {}  # maps category to category-specific model
-    model_dict["mug"] = 1
+    categories = ["bottle", "bowl", "camera", "can", "laptop", "mug"]
+    pipeline_dict = {}  # maps category to category-specific pipeline
+
+    # create per-categry models
+    for category in categories:
+        if category in config["category_configs"]:
+            category_config = yoco.load_config(
+                config["category_configs"][category], copy.deepcopy(config)
+            )
+            pipeline_dict[category] = SDFPipeline(category_config)
 
     nocs_file_names = sorted(os.listdir(os.path.join(config["data_path"], "nocs_det")))
 
@@ -41,16 +50,14 @@ def main() -> None:
         )
         rgb, depth, _, _ = load_real275_rgbd(rgb_file_path)
         masks = nocs_dict["pred_mask"]  # (rows, cols, num_masks,), binary masks
-        category_ids = nocs_dict["pred_class_ids"] - 1 # (num_masks,), class ids
-
-        print(nocs_dict["gt_handle_visibility"]) 
+        category_ids = nocs_dict["pred_class_ids"] - 1  # (num_masks,), class ids
 
         for mask_id, category_id in enumerate(category_ids):
             category = categories[category_id]  # name of category
-            mask = masks[:,:,mask_id]  # (rows, cols,)
+            mask = masks[:, :, mask_id]  # (rows, cols,)
 
             # skip unsupported category
-            if category not in model_dict:
+            if category not in pipeline_dict:
                 continue
 
             # apply estimation
