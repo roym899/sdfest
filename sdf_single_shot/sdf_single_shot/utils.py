9eb3f99f751a89b0b5bd2d9ebc9f0c6600c84e8f
Leonard Bruns <leonardb@kth.se>
2021-10-24 16:46:03 +0200

    Add str_to_object function to sdf_single_shot.utils

--- sdf_single_shot/sdf_single_shot/utils.py
+++ sdf_single_shot/sdf_single_shot/utils.py
@@ -1,5 +1,36 @@
 """General functions for experiments and pytorch."""
+import inspect
+from pydoc import locate
+import sys
+
 import torch
+from typing import Any
+
+
+def str_to_object(name: str) -> Any:
+    """Try to find object with a given name.
+
+    First scope of calling function is checked for the name, then current environment
+    (in which case name has to be a fully qualified name). In the second case, the
+    object is imported if found.
+
+    Args:
+        name: Name of the object to resolve.
+    Returns:
+        The object which the provided name refers to. None if no object was found.
+    """
+    # check callers local variables
+    caller_locals = inspect.currentframe().f_back.f_locals
+    if name in caller_locals:
+        return caller_locals[name]
+
+    # check callers global variables (i.e., imported modules etc.)
+    caller_globals = inspect.currentframe().f_back.f_globals
+    if name in caller_globals:
+        return caller_globals[name]
+
+    # check environment
+    return locate(name)
 
 
 def save_checkpoint(path: str, model: torch.nn.Module, optimizer, iteration, run_name):
