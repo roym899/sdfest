8a8abe4c54541f7a6600e595029c2ecfca6a73da
thecatfather <rbongole@gmail.com>
2021-11-08 23:27:40 +0100

    change comments

--- sdf_single_shot/tests/test_iterative_pointnet.py
+++ sdf_single_shot/tests/test_iterative_pointnet.py
@@ -2,10 +2,10 @@ import torch
 
 
 from sdf_single_shot.pointnet import VanillaPointNet, IteratativePointNet
-    
+
 
 def test_shape_equality_pointnets():
-    """Test whether VanillaPointnet and IterativePointnet with num_concat = 1 outputs same result"""
+    """Test whether VanillaPointnet and IterativePointnet outputs same shape"""
     inp1 = torch.randn(2, 500, 3)
 
     pointnet = VanillaPointNet(3, [64, 64, 1024], True)
@@ -16,6 +16,7 @@ def test_shape_equality_pointnets():
 
     assert out_p.shape == out_ip.shape
 
+    """Test whether IterativePointnet outputs required shape"""
     inp2 = torch.randn(100, 50, 2)
     iteratative_pointnet2 = IteratativePointNet(3, 2, [32, 64, 64, 1024], True)
     out_ip2 = iteratative_pointnet2(inp2)
@@ -24,6 +25,7 @@ def test_shape_equality_pointnets():
 
 
 def test_backward():
+    """Test whether backward function is possible to call"""
     iteratative_pointnet = IteratativePointNet(4, 3, [64, 64, 64, 128], True)
     inp = torch.randn(100, 500, 3)
     out1 = iteratative_pointnet(inp)
