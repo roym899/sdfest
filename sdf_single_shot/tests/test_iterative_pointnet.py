a8025e275290ca759c1f572ccef7e93352c130d4
thecatfather <rbongole@gmail.com>
2021-11-12 14:36:07 +0100

    Fix typo in test_iterative_pointnet.py

--- sdf_single_shot/tests/test_iterative_pointnet.py
+++ sdf_single_shot/tests/test_iterative_pointnet.py
@@ -1,7 +1,7 @@
 import torch
 
 
-from sdf_single_shot.pointnet import VanillaPointNet, IteratativePointNet
+from sdf_single_shot.pointnet import VanillaPointNet, IterativePointNet
 
 
 def test_shape_equality_pointnets() -> None:
@@ -12,15 +12,15 @@ def test_shape_equality_pointnets() -> None:
     pointnet = VanillaPointNet(3, [64, 64, 1024], True)
     out_p = pointnet(inp1)
 
-    iteratative_pointnet = IteratativePointNet(0, 3, [64, 64, 1024], True)
-    out_ip = iteratative_pointnet(inp1)
+    iterative_pointnet = IterativePointNet(0, 3, [64, 64, 1024], True)
+    out_ip = iterative_pointnet(inp1)
 
     assert out_p.shape == out_ip.shape
 
     """Test whether IterativePointnet outputs required shape"""
     inp2 = torch.randn(100, 50, 2)
-    iteratative_pointnet2 = IteratativePointNet(3, 2, [32, 64, 64, 1024], True)
-    out_ip2 = iteratative_pointnet2(inp2)
+    iterative_pointnet2 = IterativePointNet(3, 2, [32, 64, 64, 1024], True)
+    out_ip2 = iterative_pointnet2(inp2)
 
     assert out_ip2.shape == (100, 1024)
 
@@ -28,9 +28,9 @@ def test_shape_equality_pointnets() -> None:
 def test_backward() -> None:
     """Test whether backward function is possible to call."""
 
-    iteratative_pointnet = IteratativePointNet(4, 3, [64, 64, 64, 128], True)
+    iterative_pointnet = IterativePointNet(4, 3, [64, 64, 64, 128], True)
     inp = torch.randn(100, 500, 3)
-    out1 = iteratative_pointnet(inp)
+    out1 = iterative_pointnet(inp)
     out_sum = torch.sum(out1)
 
     out_sum.backward()
