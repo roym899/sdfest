8f7ca57412176f7d983d8f40c9be05157e566035
Leonard Bruns <leonardb@kth.se>
2021-11-18 19:06:14 +0100

    Add residual and dense options to PointNet, add new models and improve documentation

--- sdf_single_shot/sdf_single_shot/pointnet.py
+++ sdf_single_shot/sdf_single_shot/pointnet.py
@@ -18,7 +18,7 @@ class VanillaPointNet(nn.Module):
         mlp_out_sizes: List,
         batchnorm: bool,
         residual: bool = False,
-        tnet: Optional[dict] = None,
+        dense: bool = False,
     ):
         """Initialize the VanillaPointNet module.
 
@@ -36,6 +36,8 @@ class VanillaPointNet(nn.Module):
         self._in_size = in_size
         self._mlp_out_sizes = mlp_out_sizes
         self._batchnorm = batchnorm
+        self._residual = residual
+        self._dense = dense
 
         # define layers
         self._linear_layers = torch.nn.ModuleList([])
@@ -43,41 +45,19 @@ class VanillaPointNet(nn.Module):
             if i == 0:
                 self._linear_layers.append(nn.Linear(self._in_size, out_size))
             else:
-                self._linear_layers.append(nn.Linear(mlp_out_sizes[i - 1], out_size))
+                if dense:
+                    self._linear_layers.append(
+                        nn.Linear(2 * mlp_out_sizes[i - 1], out_size)
+                    )
+                else:
+                    self._linear_layers.append(
+                        nn.Linear(mlp_out_sizes[i - 1], out_size)
+                    )
 
         self._bn_layers = torch.nn.ModuleList([])
         if self._batchnorm:
             for out_size in mlp_out_sizes:
                 self._bn_layers.append(nn.BatchNorm1d(out_size))
-        self._end_1 = nn.Linear(mlp_out_sizes[-1], 1)
-
-        # define layers
-        self._linear_layers_2 = torch.nn.ModuleList([])
-        for i, out_size in enumerate(mlp_out_sizes):
-            if i == 0:
-                self._linear_layers_2.append(nn.Linear(self._in_size, out_size))
-            else:
-                self._linear_layers_2.append(nn.Linear(mlp_out_sizes[i - 1], out_size))
-
-        self._bn_layers_2 = torch.nn.ModuleList([])
-        if self._batchnorm:
-            for out_size in mlp_out_sizes:
-                self._bn_layers_2.append(nn.BatchNorm1d(out_size))
-
-        # define layers
-        self._linear_layers_3 = torch.nn.ModuleList([])
-        for i, out_size in enumerate(mlp_out_sizes):
-            if i == 0:
-                self._linear_layers_3.append(nn.Linear(self._in_size + mlp_out_sizes[-1], out_size))
-            else:
-                self._linear_layers_3.append(nn.Linear(mlp_out_sizes[i - 1], out_size))
-
-        self._bn_layers_3 = torch.nn.ModuleList([])
-        if self._batchnorm:
-            for out_size in mlp_out_sizes:
-                self._bn_layers_3.append(nn.BatchNorm1d(out_size))
-
-        # define t-net layers
 
     def forward(self, x):
         """Forward pass of the module.
@@ -88,7 +68,8 @@ class VanillaPointNet(nn.Module):
         Args:
             x: batch of point sets
         """
-        out = x
+        set_size = x.shape[1]
+        out = prev_out = x
         for i, linear_layer in enumerate(self._linear_layers):
             out = linear_layer(out)
             if self._batchnorm:
@@ -98,46 +79,18 @@ class VanillaPointNet(nn.Module):
                 out = self._bn_layers[i](out_view)
                 out = out.view(-1, pts_per_set, self._mlp_out_sizes[i])
             out = nn.functional.relu(out)
-        global_feat, _ = torch.max(out, 1, keepdim=True)
-        set_size = x.shape[1]
-        print(x.shape)
-        print(global_feat.shape)
-        out = torch.cat((x, global_feat.expand(-1, set_size, -1)), dim=2)
 
-        for i, linear_layer in enumerate(self._linear_layers_3):
-            out = linear_layer(out)
-            if self._batchnorm:
-                # BN over channels across all points and sets
-                pts_per_set = out.shape[1]
-                out_view = out.view(-1, self._mlp_out_sizes[i])
-                out = self._bn_layers_3[i](out_view)
-                out = out.view(-1, pts_per_set, self._mlp_out_sizes[i])
-            out = nn.functional.relu(out)
-        factors = torch.sigmoid(self._end_1(out))
+            if self._dense:
+                out_max, _ = torch.max(out, 1, keepdim=True)
+                if i != len(self._linear_layers) - 1:
+                    out = torch.cat((out, out_max.expand(-1, set_size, -1)), dim=2)
 
-        out = x
-        for i, linear_layer in enumerate(self._linear_layers_2):
-            out = linear_layer(out)
-            if self._batchnorm:
-                # BN over channels across all points and sets
-                pts_per_set = out.shape[1]
-                out_view = out.view(-1, self._mlp_out_sizes[i])
-                out = self._bn_layers_2[i](out_view)
-                out = out.view(-1, pts_per_set, self._mlp_out_sizes[i])
-            out = nn.functional.relu(out)
-
-        # multiply features by factors
-        out = out * factors
+            if self._residual:
+                if prev_out.shape == out.shape:
+                    out = prev_out + out
+            prev_out = out
 
         # Maximum over points in same set
         out, _ = torch.max(out, 1)
 
         return out
-
-
-if __name__ == "__main__":
-    # simple sanity check
-    pointnet = VanillaPointNet(3, [64, 64, 64, 128, 1024], True)
-    inp = torch.randn(1, 500, 3)
-    out = pointnet(inp)
-    print(out.shape)
