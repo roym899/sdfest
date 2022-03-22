e5eb44774059345d1ace1c4c93010398c85b78c3
Leonard Bruns <leonardb@kth.se>
2022-03-22 18:54:44 +0100

    Fix VAE training script

--- reproduce_scripts/train_vaes.sh
+++ reproduce_scripts/train_vaes.sh
@@ -1,6 +1,6 @@
-python -m sdfest.vae.scripts.train --config sdfest.initialization/vae_models/bowl.yaml --dataset_path ./data/shapenet_processed/bowl_filtered/ &
-python -m sdfest.vae.scripts.train --config sdfest.initialization/vae_models/bottle.yaml --dataset_path ./data/shapenet_processed/bottle_filtered/ &
-python -m sdfest.vae.scripts.train --config sdfest.initialization/vae_models/mug.yaml --dataset_path ./data/shapenet_processed/mug_filtered/ &
-python -m sdfest.vae.scripts.train --config sdfest.initialization/vae_models/camera.yaml --dataset_path ./data/shapenet_processed/camera_filtered/ &
-python -m sdfest.vae.scripts.train --config sdfest.initialization/vae_models/laptop.yaml --dataset_path ./data/shapenet_processed/laptop_filtered/ &
-python -m sdfest.vae.scripts.train --config sdfest.initialization/vae_models/can.yaml --dataset_path ./data/shapenet_processed/can_filtered/ &
+python -m sdfest.vae.scripts.train --config ./sdfest/initialization/vae_models/bowl.yaml --dataset_path ./data/shapenet_processed/bowl_filtered/ &
+python -m sdfest.vae.scripts.train --config ./sdfest/initialization/vae_models/bottle.yaml --dataset_path ./data/shapenet_processed/bottle_filtered/ &
+python -m sdfest.vae.scripts.train --config ./sdfest/initialization/vae_models/mug.yaml --dataset_path ./data/shapenet_processed/mug_filtered/ &
+python -m sdfest.vae.scripts.train --config ./sdfest/initialization/vae_models/camera.yaml --dataset_path ./data/shapenet_processed/camera_filtered/ &
+python -m sdfest.vae.scripts.train --config ./sdfest/initialization/vae_models/laptop.yaml --dataset_path ./data/shapenet_processed/laptop_filtered/ &
+python -m sdfest.vae.scripts.train --config ./sdfest/initialization/vae_models/can.yaml --dataset_path ./data/shapenet_processed/can_filtered/ &
